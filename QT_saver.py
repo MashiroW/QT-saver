ff93b735c4139593b2bc3ddacf6f4a9ea2a0aee3
YouYou <47890166+MashiroW@users.noreply.github.com>
2023-01-01 23:32:12 +0100

    Add files via upload

---
+++ QT_saver.py
@@ -0,0 +1,87 @@
+from configparser import ConfigParser
+import pyperclip
+import requests
+import json
+import os
+
+# Application configuration
+app_config = ConfigParser()
+app_config.read("config.ini")
+SYSTEM_SECTION_APP    = app_config.sections()[0]
+SAVEPATH              = str(app_config.get(SYSTEM_SECTION_APP, "SAVEPATH"))
+
+def getURL(id = "None"):
+
+    if id == "None":
+        ACCOUNT_ID            = str(app_config.get(SYSTEM_SECTION_APP, "USERID"))
+
+    else:
+        ACCOUNT_ID = id
+
+    URL = "https://www.instagram.com/graphql/query/?query_hash=de8017ee0a7c9c45ec4260733d81ea31&variables=%7B%22reel_ids%22%3A%5B%22{0}%22%5D%2C%22tag_names%22%3A%5B%5D%2C%22location_ids%22%3A%5B%5D%2C%22highlight_reel_ids%22%3A%5B%5D%2C%22precomposed_overlay%22%3Afalse%2C%22show_story_viewer_list%22%3Atrue%2C%22story_viewer_fetch_count%22%3A50%2C%22story_viewer_cursor%22%3A%22%22%7D".format(ACCOUNT_ID)
+
+    return URL
+
+def welcome():
+    print("------TURBO CUTIE SAVER 3000-----")
+    print("---------------------------------")
+    print("Open the following URL in your browser that's already logged in to Instagram:\n")
+    print(getURL(), "\n")
+
+    print("Now copy to your clipboard the JSON API response then press ENTER")
+    os.system("Pause")
+
+def parser():
+    urls = []
+    src_dict = json.loads(pyperclip.paste())
+    count_vids = 0
+    count_pics = 0
+
+    for element_idx in range (0, len(src_dict["data"]["reels_media"][0]["items"])):
+
+        # - If video
+        if src_dict["data"]["reels_media"][0]["items"][element_idx]["is_video"] == True:
+            print("ELEMENT #{} (VID):".format(element_idx+1))
+            url = src_dict["data"]["reels_media"][0]["items"][element_idx]["video_resources"][0]["src"]
+            count_vids +=1
+
+        # - If picture
+        else:
+            print("ELEMENT #{} (PIC):".format(element_idx+1))
+            url = src_dict["data"]["reels_media"][0]["items"][element_idx]["display_url"]   
+            count_pics +=1      
+
+        urls.append(url)
+        #print(url, "\n")
+
+    return urls, count_pics, count_vids
+
+def saver(url):
+    response = requests.get(url)
+
+    if ".mp4" in url:
+        type = ".mp4"
+    else:
+        type = ".jpg"
+
+    filename = SAVEPATH + "/" + url.split(type)[0].split("/")[-1] + type
+    open(filename, "wb").write(response.content)
+
+def get_content():
+    urls, _, _, = parser()
+
+    try:
+        os.makedirs(SAVEPATH)
+    except FileExistsError:
+        # directory already exists
+        pass
+
+    for url in urls:
+        saver(url)
+
+if __name__ == "__main__":
+    get_content()
+
+
+
+    
---
+++ gui.py
@@ -0,0 +1,171 @@
+from QT_saver import *
+from tkinter import *
+from PIL import ImageTk, Image
+import webbrowser
+import pyperclip
+import os
+
+class MyWindow:
+    def __init__(self, win):
+
+        self.URL = ""
+
+        self.GENERAL_X = 6
+        self.SECTION1_Y = 40
+        self.SECTION2_Y = 180
+        self.SECTION3_Y = 330
+
+        self.BUTTONS_Y = 70
+
+        self.title=Label(window, text="Download some user's Instagram public&private\nstories at once !", fg='#4ee44e', font=("Segoe UI Symbol", 10), bg = 'black')
+        self.title.place(x=self.GENERAL_X, y=10)
+
+        # - SIGNATURE
+
+        self.signature=Label(window, text="BY MASH ", fg='black', bg='white', font=("Leelawadee", 8, "bold"))
+        self.signature.place(x=self.GENERAL_X + 239,
+                           y=self.SECTION3_Y + 185)
+
+        # - QT PIC
+        """
+        self.widget = Label(window, compound='bottom')
+        self.widget.place(x=self.GENERAL_X,
+                          y=self.SECTION3_Y + 160)
+        self.widget.lenna_image_png = PhotoImage(file="splash-removebg-preview.png")
+        self.widget['image'] = self.widget.lenna_image_png
+        self.widget.pack()
+        """
+
+        self.image1 = Image.open("splash-removebg-preview.png")
+        newsize = (60, 60)
+        self.image1 = self.image1.resize(newsize)
+        self.test = ImageTk.PhotoImage(self.image1)
+
+        self.label1 = Label(image=self.test, width=51, height=50)
+        self.label1.image = self.test
+        self.label1.place(x=245, y=465)
+
+
+
+        # - SECTION 1
+        self.section1_title=Label(window, text="STEP 1 - NOT COMPLETE", fg='white', font=("Segoe UI Symbol", 10), bg = 'red')
+        self.section1_title.place(x=self.GENERAL_X, 
+                                  y=self.SECTION1_Y + 30)
+
+        self.btn01=Button(window, text="Use the USERID\n in the .ini file", fg='white', bg='#7a8793', command=self.click_btn01)
+        self.btn01.place(x=self.GENERAL_X + 20,
+                         y=self.BUTTONS_Y + self.SECTION1_Y)
+
+        self.btn02=Button(window, text="Use the USERID\nyou wrote below", fg='white', bg='#7a8793', command=self.click_btn02)
+        self.btn02.place(x=self.GENERAL_X + 170,
+                         y=self.BUTTONS_Y + self.SECTION1_Y)
+
+        self.txtfld=Entry(window, text="Write here your user ID", bd=2)
+        self.txtfld.place(x=self.GENERAL_X + 155,
+                          y=self.BUTTONS_Y + self.SECTION1_Y + 60)
+
+        # - SECTION 2
+
+        self.section2_title=Label(window, text="STEP 2 - NOT COMPLETE", fg='white', font=("Segoe UI Symbol", 10), bg = 'red')
+        self.section2_title.place(x=self.GENERAL_X, 
+                                  y=self.SECTION2_Y + 30)
+
+        self.btn03=Button(window, text="Copy URL to\nclipboard", fg='white', bg='#7a8793', command=self.click_btn03)
+        self.btn03.place(x=self.GENERAL_X + 110,
+                         y=self.BUTTONS_Y + self.SECTION2_Y)
+
+        self.hint01=Label(window, text="Paste the URL once copied in your browser\nwhere you're already logged into Instagram", fg='red', bg='black', font=("Segoe UI Symbol", 8))
+        self.hint01.place(x=self.GENERAL_X + 35, y=self.SECTION2_Y + 120)
+
+        # - SECTION 3
+
+        self.section3_title=Label(window, text="STEP 3 - NOT COMPLETE", fg='white', font=("Segoe UI Symbol", 10), bg = 'red')
+        self.section3_title.place(x=self.GENERAL_X, 
+                                  y=self.SECTION3_Y + 30)
+
+        self.hint02=Label(window, text="Copy the entire response content from\nyour browser before clicking this button", fg='red', bg='black', font=("Segoe UI Symbol", 8))
+        self.hint02.place(x=self.GENERAL_X + 35, y=self.SECTION3_Y + 65)
+
+        self.btn04=Button(window, text="Paste & run", fg='white', bg='#7a8793', command=self.click_btn04)
+        self.btn04.place(x=self.GENERAL_X + 110,
+                         y=self.BUTTONS_Y + self.SECTION3_Y + 35)
+
+        self.infomsg=Label(window, text="Idling...", fg='yellow', bg='black', font=("Segoe UI Symbol", 8))
+        self.infomsg.place(x=self.GENERAL_X + 5,
+                           y=self.SECTION3_Y + 160)
+
+
+
+
+
+    def click_btn01(self):
+        # - STYLE
+        self.section1_title["bg"]="green"
+        self.section2_title["bg"]="red"
+        self.section3_title["bg"]="red"
+        self.section1_title["text"]="STEP 1 - COMPLETE"
+        self.section2_title["text"]="STEP 2 - NOT COMPLETE"
+        self.section3_title["text"]="STEP 3 - NOT COMPLETE"
+        self.infomsg["text"]="Idling..."
+        # - ---
+
+        self.URL = getURL()
+        
+    def click_btn02(self):
+        # - STYLE
+        self.section1_title["bg"]="green"
+        self.section2_title["bg"]="red"
+        self.section3_title["bg"]="red"
+        self.section1_title["text"]="STEP 1 - COMPLETE"
+        self.section2_title["text"]="STEP 2 - NOT COMPLETE"
+        self.section3_title["text"]="STEP 3 - NOT COMPLETE"
+        self.infomsg["text"]="Idling..."
+        # - ---
+
+        field_txt = self.txtfld.get()
+        self.URL = getURL(id = field_txt)
+
+    def click_btn03(self):
+        # - STYLE
+        self.section2_title["bg"]="green"
+        self.section3_title["bg"]="red"
+        self.section2_title["text"]="STEP 2 - COMPLETE"
+        self.section3_title["text"]="STEP 3 - NOT COMPLETE"
+        self.infomsg["text"]="Idling..."
+        # - ---
+
+        pyperclip.copy(self.URL)
+
+    def click_btn04(self):
+        # - STYLE
+        self.section3_title["bg"]="green"
+        self.section3_title["text"]="STEP 3 - COMPLETE"
+        # - ---
+
+        self.infomsg["text"]="Processing..."
+        Tk.update(window)
+
+        urls, count_pics, count_vids, = parser()
+
+        try:
+            os.makedirs(SAVEPATH)
+        except FileExistsError:
+            # directory already exists
+            pass
+
+        for url in urls:
+            saver(url)
+
+        self.infomsg["text"]="Done. {0} pics and {1} vids have successfully\nbeen saved.".format(count_pics, count_vids)
+
+
+window=Tk()
+MyWindow(window)
+window.iconbitmap("splash-removebg-preview.ico")
+window.title('QT saver')
+window.geometry("300x530+10+10")
+window['background']='#000000'
+window.resizable(False, False) 
+
+
+window.mainloop()
